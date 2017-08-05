18d8927a977f1d54f146ba9b763644a199525d86
Arpit Nandwani <arpit.nandwani@gmail.com>
2017-08-06 03:21:30 +0530

    Added create team page

--- Incubate/views/event.py
+++ Incubate/views/event.py
@@ -1,6 +1,6 @@
 from Incubate.views.home import *
 
-@app.route('/' + session['event'], methods=['GET', 'POST'])
+@app.route('/event', methods=['GET', 'POST'])
 @login_required
 def event():
     event = session['event']
@@ -15,3 +15,30 @@ def event():
 
         render_template('event.html', event = event)
 
+
+@app.route('/createteam', methods=['GET', 'POST'])
+def createteam():
+    if request.method == 'POST':
+        event = request.args.get('event')
+
+        table = event + 'team'
+
+        db = mysql.connection.cursor()
+        db.execute("SELECT email FROM users WHERE id = '{}'".format(session['user_id']))
+        email = db.fetchone()
+
+        db.execute(
+            "INSERT IGNORE INTO " + table +" (team_name, user1, user2, user3, user4, user5) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')".format(
+            request.form.get("team_name"), email, request.form.get("user2"), request.form.get("user3"), request.form.get("user4"), request.form.get("user5")))
+        mysql.connection.commit()
+
+        return redirect(url_for('questions', event = event))
+
+    else:
+        event = request.args.get('event')
+        return render_template('createteam.html', event = event)
+
+
+@app.route('/questions', methods=['GET', 'POST'])
+def questions():
+    return render_template('failure.html')
