fb5032d4c446af33b4683af8d434587fb6f4ed3e
Arpit Nandwani <arpit.nandwani@gmail.com>
2017-08-06 03:21:59 +0530

    Added Join Team Page

--- Incubate/views/event.py
+++ Incubate/views/event.py
@@ -38,6 +38,50 @@ def createteam():
         event = request.args.get('event')
         return render_template('createteam.html', event = event)
 
+@app.route('/jointeam', methods=['GET', 'POST'])
+def jointeam():
+    event = str(request.args.get('event'))
+    table = event + 'team'
+    db = mysql.connection.cursor()
+
+    if request.method == 'POST':
+
+            rows = db.execute("SELECT * FROM " + table + " WHERE team_name = '{}'".format(request.form.get("team")))
+            if rows is 0:
+                return render_template('failure.html', msg = "Team not found")
+            team = db.fetchone()
+            if team['user2'] == '':
+                user = 'user2'
+            elif team['user3'] == '':
+                user = 'user3'
+            elif team['user4'] == '':
+                user = 'user4'
+            elif team['user5'] == '':
+                user = 'user5'
+            else:
+                return render_template('failure.html', msg='Sorry, this team is already filled')
+
+            db.execute("UPDATE " + table + " SET " + user + " = '{0}' WHERE team_name = '{1}'".format(request.form.get("email"), team['team_name']))
+            mysql.connection.commit()
+            return redirect(url_for('questions', event = event))
+
+
+    else:
+        db.execute("SELECT team_name, user5 FROM " + table)
+        rows = db.fetchall()
+        teams = list()
+        for row in rows:
+            temp = dict()
+            temp['team_name'] = row['team_name']
+            if row['user5'] == '':
+                temp['availability'] = True
+            else:
+                temp['availability'] = False
+            teams.append(temp)
+
+        return render_template('jointeam.html', teams = teams, event = event)
+
+
 
 @app.route('/questions', methods=['GET', 'POST'])
 def questions():
